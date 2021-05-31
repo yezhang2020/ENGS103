2a73328ded9e4a72338ef5b87bac0ac2bf01d5ae
tqi-T <69809039+tqi-T@users.noreply.github.com>
2021-05-31 13:20:21 -0400

    Update Project.m

--- Project.m
+++ Project.m
@@ -1,47 +1,63 @@
-%% MM2 registration
-lambda=1/3;
-mu=1/5;
-m=2;
-q=25;
-p=zeros(q,1);
-p0=(1-lambda/(2*mu))/(1+lambda/(2*mu));
-for i=1:q
-    p(i)=2*(lambda/2/mu)^i*p0;
+%% MMm registration
+lambda=1/1.11;
+mu=1/4;
+m=8;
+q=200;
+denom_p0=0;
+p=zeros(q+1,1);
+for i=0:q
+    if i<=m
+        p(i+1)=1/factorial(i)*(lambda/mu)^i;
+    else
+        p(i+1)=1/(m^(i-m)*factorial(m))*(lambda/mu)^i;
+    end
+    denom_p0=denom_p0+p(i+1);
 end
-nn=1:1:25;
+p0=1/denom_p0;
+p=p*p0;
+nn=0:1:q;
 L=sum(p.*nn');
 W=L/lambda;
 
-%% MM3 registration
-lambda=1/3;
-mu=1/5;
+%% MMn vaccination, with m registration, 0 queue capacity (no queue when n>5)
 m=3;
-q=25;
-p=zeros(q,1);
-p0=1/(1+lambda/mu+0.5*(lambda/mu)^2*(1/(lambda/(3*mu))));
-p(1)=lambda/mu*p0;
-p(2)=lambda/(2*mu)*p(1);
-for i=3:q
-    p(i)=lambda/(3*mu)*p(i-1);
-end
-nn=1:1:25;
-L=sum(p.*nn');
-W=L/lambda;
-
-%% MM5 vaccination, with 2 registration, 0 queue capacity?
-lambda=1/(5*m);
-mu=1/8;
-n=5;
+n=5; % # of vaccination station
+lambda=m/4;
+mu=1/3;
 dem_p0=0;
 p=zeros(1,n);
 for i=0:n
-    dem_p0=dem_p0+(lambda/mu)^i/factorial(i);
+    p(i+1)=(lambda/mu)^i/factorial(i);
+    dem_p0=dem_p0+p(i+1);
 end
 p0=1/dem_p0;
-for i=1:n
-    p(i)=(lambda/mu)^i/factorial(i)*p0;
-end
+p=p*p0;
 
-nn=1:1:n;
+nn=0:1:n;
 L=sum(p.*nn);
 W=L/lambda;
+
+%% MMn vaccination, with m registration, with infinite queue capacity
+m=4;
+n=7;
+lambda=m/4;
+mu=1/3;
+
+q=200;
+denom_p0=0;
+p=zeros(q+1,1);
+for i=0:q
+    if i<=n
+        p(i+1)=1/factorial(i)*(lambda/mu)^i;
+    else
+        p(i+1)=1/(n^(i-n)*factorial(n))*(lambda/mu)^i;
+    end
+    denom_p0=denom_p0+p(i+1);
+end
+p0=1/denom_p0;
+p=p*p0;
+nn=0:1:q;
+L=sum(p.*nn');
+W=L/lambda;
+Wq=W-1/mu;
+Lq=lambda*Wq;
